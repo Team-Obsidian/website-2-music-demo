1db1cf663176bd2e80da51a4e3226bcb0d0a9adc
TeamObsidianAsterisk <5065056-TeamObsidianAsterisk@users.noreply.replit.com>
2023-06-11 22:59:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Load Node modules
+var express = require('express');
+// Initialise Express
+var app = express();
+// Render static files
+app.use(express.static('public'));
+// Port website will run on
+app.listen(8080);
+
+
+
+
+var cors = require('cors')
+app.use(cors())
+ 
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+ 
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
